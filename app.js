232d14aa86d0c5e0885a24ab3adc7571ca7d9f6e
William Yeung <williamyeung1141998@gmail.com>
2019-09-29 23:49:10 -0400

    Need to implement "Reset Game"/"Play Again" feature

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const qwerty = document.getElementById("qwerty");
 const ul = document.querySelector("#phrase ul");
 const overlay = document.getElementById("overlay");
+const startBtn = document.getElementsByClassName("btn__start")[0];
 const resetBtn = document.getElementsByClassName("btn__reset")[0];
+const title = document.querySelector(".title");
 let missed = 0;
-
+let classLetter = 0;
+let classShow = 0;
+let hearts = document.querySelectorAll(".tries")
+let hearPosition = 0;
 
 
 const names = ["William", "Betty", "Jade", "Joseph", "Jasmine", "Jessie"];
@@ -39,11 +44,14 @@ function addPhraseToDisplay(arr) {
         li.textContent = arr[i];
 
         if (arr[i] !== " ") {
-            li.classList.add("letter")
+            li.classList.add("letter");
+            classLetter += 1;
         }
         ul.appendChild(li);
     };
 };
+
+
 
 
 
@@ -54,11 +62,22 @@ function addPhraseToDisplay(arr) {
 // of the li's, and inside of the actual word
 function getAllOccurences(array, valueToFind) {
     let values = [];
+    
     for (let i = 0; i < array.length; i++) {
-        if (array[i].textContent.toLowerCase() === valueToFind.textContent.toLowerCase()) {
+        if (array[i].textContent.toLowerCase() === valueToFind.textContent) {
             values.push(array[i])
-        }
+        } 
     }
+
+    // if the players guesses a wrong character, then the values array will obviously have nothing
+    // this will cause a heart(life) to decrease by 1
+    if (values.length === 0) {
+        missed += 1;
+        let lives = document.querySelectorAll(".tries");
+        lives[hearPosition].style.display = "none";
+        hearPosition++
+    }
+
     return values;
 }
 
@@ -71,9 +90,29 @@ function getAllOccurences(array, valueToFind) {
 function showLetter(arrayOfCorrectLetters) {
     for (let i = 0; i < arrayOfCorrectLetters.length; i++) {
         arrayOfCorrectLetters[i].classList.add("show");
+        classShow += 1;
     }
 }
 
+
+
+
+
+function checkStatus() {
+    if (missed === 5) {
+        overlay.style.display = "";
+        overlay.classList.add("lose")
+        title.textContent = "Game Over!!!"
+        resetBtn.style.display = "block";
+        startBtn.style.display = "none";
+    } else if (classShow === classLetter) {
+        overlay.style.display = "";
+        overlay.classList.add("win");
+        title.textContent = "You Won!!!"
+        resetBtn.style.display = "block";
+        startBtn.style.display = "none";
+    }
+}
 
 
 
@@ -83,46 +122,40 @@ qwerty.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
         e.target.classList.add("chosen")
         e.target.disabled = true;
+        let lettersFound = getAllOccurences(ul.children, e.target);
+        showLetter(lettersFound)
+        checkStatus();
     }
-
-    let lettersFound = getAllOccurences(ul.children, e.target);
-    showLetter(lettersFound)
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-resetBtn.addEventListener("click", () => {
-     overlay.style.display = "none"
+startBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
 });
 
-
+resetBtn.addEventListener("click", (e) => {
+    overlay.style.display = "none";
+    let keyButtons = qwerty.querySelectorAll("button");
+    for (let i = 0; i < keyButtons.length; i++) {
+        keyButtons[i].disabled = false;
+        keyButtons[i].classList.remove("chosen");
+        missed = 0;
+        classLetter = 0;
+        classShow = 0;
+        hearPosition = 0;
+        for (let i = 0; i < hearts.length; i++) {
+            hearts[i].style.display = "";
+        }
+    }
+ 
+})
 
 
 
 const phraseArray = getRandomPhraseAsArray(names)
 addPhraseToDisplay(phraseArray)
+
 
 
